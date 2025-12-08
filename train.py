24de6b2e12ca47c8f6ea1b8c028b713434ce390c
zyy555 <104243192+zyy555@users.noreply.github.com>
2025-12-08 18:59:24 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,10 +1,6 @@
-# This is a sample Python script.
 from pickle import FALSE
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 import numpy as np
-
+import pandas as pd
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -16,7 +12,7 @@ from sklearn.metrics import roc_curve, precision_recall_curve, average_precision
 from sklearn.metrics import auc
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, classification_report,matthews_corrcoef,f1_score
-import capsule_net_final
+import model
 
 USE_CUDA = torch.cuda.is_available()
 
@@ -30,15 +26,9 @@ def cal_acc(y_true,X_test,net):
         if USE_CUDA:
             batch_x = batch_x.cuda()
         output, reconstructions, masked = net(batch_x)
-        #score = logit.squeeze(1).cpu().detach().numpy().tolist()
-        #all_score += score
-        #pred_labels = np.argmax(masked.data.cpu().numpy(), 1)
         pred  = np.argmax(masked.cpu().detach().numpy(),axis=1).tolist()
-        # print(pred)
         all_pred += pred
     tn, fp, fn, tp = confusion_matrix(y_test, all_pred).ravel()
-    #fpr, tpr, _ = roc_curve(test_labels, all_pred)
-    #aucroc = auc(fpr, tpr)
     perftab = {"CM": confusion_matrix(y_test, all_pred),
             'ACC': (tp + tn) / (tp + fp + fn + tn),
             'SEN': tp / (tp + fn),
@@ -57,12 +47,11 @@ if USE_CUDA:
     capsule_net = capsule_net.cuda()
 optimizer = Adam(capsule_net.parameters(),lr=0.01,betas=(0.9,0.999))
 
-import numpy as np
-import pandas as pd
+
 batch_size = 64
 n_epochs = 30
 res = 64
-fig = pd.read_csv("res64.txt")
+fig = pd.read_csv("/data/FCGR/train_fcgr.txt")
 
 str_list=[]
 for i in fig['figure']:
@@ -110,23 +99,12 @@ for epoch in range(n_epochs):
         loss = capsule_net.loss(data, output, target, reconstructions)
         loss.backward()
 
-        #for name, param in capsule_net.named_parameters():
-        #    if param.grad is None:
-        #        print(f"WARNING: No gradient for {name}")  # 检查是否有梯度未更新
-        #    else:
-        #        print(f"{name} gradient norm: {param.grad.norm().item()}")  # 打印梯度的 L2 范数
-
         optimizer.step()
         train_loss += loss.item()
 
-
-
-
         # Calculate the number of correct predictions
         pred_labels = np.argmax(masked.data.cpu().numpy(), 1)
-        #print(pred_labels)
         true_labels = np.argmax(target.data.cpu().numpy(), 1)
-        #print(true_labels)
         correct_train += np.sum(pred_labels == true_labels)
 
         # Calculate TP, FN, FP for recall calculation
@@ -146,5 +124,4 @@ for epoch in range(n_epochs):
     with torch.inference_mode():
         avg_test_accuracy,recall_test,precision_test,tab=cal_acc(y_test,X_test,capsule_net)
         print(f"Test Accuracy: {avg_test_accuracy:.4f}, Test Recall: {recall_test:.4f},Test Precision:{precision_test:.4f}")
-        #torch.save(capsule_net,'model/capsule_net.pth')
         torch.save(capsule_net.state_dict(), 'model/capsule_net.pth')
